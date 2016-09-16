0b4bc9b7bfc543dc0fa1dc05d00af4c2728de5bf
CHAPELLE Quentin <chapelle.quentin@gmail.com>
2016-09-17 01:27:28 +0200

    include polymer directive on angular2

--- myWebsite/src/app/app.module.ts
+++ myWebsite/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { PolymerElement } from '@vaadin/angular2-polymer';
 
 import { AppComponent } from './app.component';
 import { CvComponent } from './cv/cv.component';
@@ -9,7 +10,12 @@ import { CvComponent } from './cv/cv.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CvComponent
+    CvComponent,
+    PolymerElement('paper-tabs'),
+    PolymerElement('paper-tab'),
+    PolymerElement('paper-header-panel'),
+    PolymerElement('paper-toolbar'),
+    PolymerElement('paper-card')
   ],
   imports: [
     BrowserModule,
@@ -17,6 +23,7 @@ import { CvComponent } from './cv/cv.component';
     HttpModule
   ],
   providers: [],
+  schemas:  [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
